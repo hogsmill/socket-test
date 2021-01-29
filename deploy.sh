f7b5139f235d402725b3a0d7e510ecba84bc0168
hogsmill <info@hogsmill.com>
2021-01-29 16:00:24 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "-f" ]; then
+  FORCE=true
+fi
+
+PORT=3016
+PWD=`pwd`
+APP=`basename $PWD`
+GIT=`git pull`
+echo $GIT
+if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
+  exit 0
+fi
+
+npm run build
+rm /var/www/html/$APP/css/*
+rm /var/www/html/$APP/js/*
+cp -R dist/* /var/www/html/$APP
+if [ -f "src/server.js" ]; then
+  SERVER=`ps -ef | grep server.js | grep $PORT | awk {'print $2'}`
+  if [ "$SERVER" != "" ]; then
+    kill $SERVER
+  fi
+fi
