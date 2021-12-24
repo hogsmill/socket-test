38e2ea09bac6a10dad8ea202dc2cb229cb37e2db
hogsmill <info@hogsmill.com>
2021-12-24 14:09:19 +0000

    modal test

---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="show()">
+      Show Modal
+    </button>
+
+    <modal name="popup" class="popup" :height="400" :classes="['rounded']">
+      <div>
+        <h4>Modal Test</h4>
+        <button class="btn btn-sm btn-info" @click="done()">
+          Done
+        </button>
+      </div>
+    </modal>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    show() {
+      console.log('show')
+      this.$modal.show('popup')
+    },
+    done() {
+      console.log('hide')
+      this.$modal.hide('popup')
+    }
+  }
+}
+</script>
